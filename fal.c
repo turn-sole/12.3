9e39ca7b49b4dd06ff5c8f4f8eba6fed7a9764ab
turn-sole <113235914+turn-sole@users.noreply.github.com>
2022-12-03 22:56:35 +0800

    Add files via upload

---
+++ fal.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+//int add(int a, int b)
+//{
+//	int d = 0;
+//	d = a + b;
+//	return d;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 0;
+//	c = add(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[20];
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//输入两个整数，范围 - 231~231 - 1，交换两个数并输出。
+//
+//输入描述：
+//输入只有一行，按照格式输入两个整数，范围，中间用“, ”分隔。
+//int main() {
+//    int a = 0;
+//    int b = 0;
+//    scanf("a=%d,b=%d", &a, &b);
+//    int i = a;
+//    a = b;
+//    b = i;
+//    printf("a=%d,b=%d", a, b);
+//    return 0;
+//}
+//实现字母的大小写转换。多组输入输出。
+//int main() {
+//    int ch = 0;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        getchar();
+//        putchar(ch + 32);
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//十六进制整数ABCDEF对应的十进制整数，所占域宽为15。
+//int main() {
+//    int a = 0xABCDEF;
+//    printf("%15d", a);
+//    return 0;
+//}
